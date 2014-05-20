5d34279bafb59de7f2182af35b65a7744d3774df
Maartyl <kuronomaartyl@gmail.com>
2014-05-21 00:09:24 +0200

    changed package object to normal package

--- src/cz/maartyl/maartyl.impls.scala
+++ src/cz/maartyl/maartyl.impls.scala
@@ -1,16 +1,15 @@
-package cz
+package cz.maartyl
 
-package object maartyl {
-  object Pipe {
-    implicit class PipedObject[A](val value: A) extends AnyVal {
-      @inline def |>[B](f: A => B): B = f(value)
-      @inline def |>[B](f: (A, A) => B): B = f(value, value)
-      @inline def |>[B](f: (A, A, A) => B): B = f(value, value, value)
-    }
+object Pipe {
+  implicit class PipedObject[A](val value: A) extends AnyVal {
+    @inline def |>[B](f: A => B): B = f(value)
+    @inline def |>[B](f: (A, A) => B): B = f(value, value)
+    @inline def |>[B](f: (A, A, A) => B): B = f(value, value, value)
   }
-  object Regex {
-    implicit class Regex(val sc: StringContext) extends AnyVal{
-      def r = new util.matching.Regex(sc.parts.mkString, sc.parts.tail map { _ => "x" }: _*)
-    }
+}
+
+object Regex {
+  implicit class Regex(val sc: StringContext) extends AnyVal {
+    def r = new util.matching.Regex(sc.parts.mkString, sc.parts.tail map { _ => "x" }: _*)
   }
 }
